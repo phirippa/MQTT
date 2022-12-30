19e3819048f181d0d98e16d921c348316b5ec8f9
김성필(Sungpil Kim) <phirippa@naver.com>
2022-12-30 20:30:15 +0900

    Update pub_topic.py

--- pub_topic.py
+++ pub_topic.py
@@ -6,11 +6,11 @@ count = 0
 while( count< 10 ):
     publish.single("MyOffice/Indoor/Temp", "23.4", hostname="localhost")
     publish.single("MyOffice/Indoor/Humi", "33", hostname="localhost")
-    publish.single("MyOffice/Indoor/Lamp", "ON", hostname="localhost")
+    publish.single("MyOffice/Indoor/Led", "ON", hostname="localhost")
 
     publish.single("MyOffice/Outdoor/Temp", "33.4", hostname="localhost")
     publish.single("MyOffice/Outdoor/Humi", "23", hostname="localhost")
-    publish.single("MyOffice/Outdoor/Lamp", "OFF", hostname="localhost")
+    publish.single("MyOffice/Outdoor/Led", "OFF", hostname="localhost")
 
     time.sleep(2)
     count+=1
