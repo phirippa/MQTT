2655f436c9ad5d6f9da8bea5148df2a6a4899e91
김성필(Sungpil Kim) <phirippa@naver.com>
2023-01-01 07:24:29 +0900

    Create subWithID.py

---
+++ subWithID.py
@@ -0,0 +1,17 @@
+
+import paho.mqtt.client as mqtt
+
+def on_connect( client, userdata, flags, rc ):
+   print("Connect with result code " + str(rc) )
+   client.subscribe("MyOffice/Indoor/#")
+
+def on_message( client, userdata, msg ):
+   print( msg.topic +" "+str(msg.payload) )
+
+client = mqtt.Client()
+client.username_pw_set(username="mqtt_ship", password="1234")
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+client.loop_forever( )
